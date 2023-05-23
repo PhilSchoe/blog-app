df5377f223efc95f00381b9dd60a1d381682f50b
PhilSchoe <p.schoemacker@gmx.de>
2023-05-23 17:53:01 +0200

    Move state to store

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,50 +1,10 @@
 <script setup lang="ts">
-import { DateTime } from 'luxon'
-import { ref, computed } from 'vue'
-import { type TimelinePost } from '../posts'
 import TimelineItem from './TimelineItem.vue'
 import { usePosts } from '@/stores/posts'
+import { periods } from '@/constants'
 
 const postsStore = usePosts()
 
-const periods = ['Today', 'This Week', 'This Month'] as const
-
-type Period = (typeof periods)[number]
-
-const selectedPeriod = ref<Period>('Today')
-
-function selectPeriod(period: Period) {
-  selectedPeriod.value = period
-}
-
-const posts = computed<TimelinePost[]>(() => {
-  return postsStore.ids.reduce((accumulator: TimelinePost[], id: string) => {
-    const post = postsStore.all.get(id)
-    
-    if (post == null || post.created == null) {
-      throw Error(`Post with id of ${id} was epexcted, but not found.`)
-    }
-
-    const transformedPost = {
-      ...post,
-      created: DateTime.fromISO(post.created)
-    }
-
-    if (selectedPeriod.value === 'Today') {
-      if (transformedPost.created >= DateTime.now().minus({ day: 1 })) {
-        accumulator.push(transformedPost)
-      }
-    } else if (selectedPeriod.value === 'This Week') {
-      if (transformedPost.created >= DateTime.now().minus({ week: 1 })) {
-        accumulator.push(transformedPost)
-      }
-    } else {
-      accumulator.push(transformedPost)
-    }
-
-    return accumulator
-  }, [])
-})
 </script>
 
 <template>
@@ -53,13 +13,13 @@ const posts = computed<TimelinePost[]>(() => {
       <a
         v-for="period of periods"
         :key="period"
-        v-bind:class="{ 'is-active': period === selectedPeriod }"
-        @click="selectPeriod(period)"
+        v-bind:class="{ 'is-active': period === postsStore.selectedPeriod }"
+        @click="postsStore.setSelectedPeriod(period)"
       >
         {{ period }}
       </a>
     </span>
 
-    <TimelineItem v-for="post of posts" :key="post.id" :post="post" />
+    <TimelineItem v-for="post of postsStore.filteredPosts" :key="post.id" :post="post" />
   </nav>
 </template>
--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,9 +1,12 @@
-import { type Post, today, thisWeek, thisMonth } from '../posts'
+import type { Period } from '@/constants'
+import { type Post, today, thisWeek, thisMonth, type TimelinePost } from '../posts'
 import { defineStore } from 'pinia'
+import { DateTime } from 'luxon'
 
 interface PostsState {
   ids: string[]
   all: Map<string, Post>
+  selectedPeriod: Period
 }
 
 export const usePosts = defineStore('posts', {
@@ -13,10 +16,44 @@ export const usePosts = defineStore('posts', {
       [today.id, today],
       [thisWeek.id, thisWeek],
       [thisMonth.id, thisMonth],
-    ])
+    ]),
+    selectedPeriod: "Today"
   }),
 
   actions: {
-    // ...
+    setSelectedPeriod(period: Period) {
+      this.selectedPeriod = period;
+    }
+  },
+
+  getters: {
+    filteredPosts: (state): TimelinePost[] => {
+      return state.ids.reduce((accumulator: TimelinePost[], id: string) => {
+        const post = state.all.get(id)
+        
+        if (post == null || post.created == null) {
+          throw Error(`Post with id of ${id} was epexcted, but not found.`)
+        }
+    
+        const transformedPost = {
+          ...post,
+          created: DateTime.fromISO(post.created)
+        }
+    
+        if (state.selectedPeriod === 'Today') {
+          if (transformedPost.created >= DateTime.now().minus({ day: 1 })) {
+            accumulator.push(transformedPost)
+          }
+        } else if (state.selectedPeriod === 'This Week') {
+          if (transformedPost.created >= DateTime.now().minus({ week: 1 })) {
+            accumulator.push(transformedPost)
+          }
+        } else {
+          accumulator.push(transformedPost)
+        }
+    
+        return accumulator
+      }, [])
+    }
   }
 })
