a6c69c8668e3c27019525d4456e5ae777b5e69df
philippschoemacker <p.schoemacker@gmx.de>
2023-05-23 21:21:32 +0200

    Add two way binding for the content. Enable text input

--- src/components/PostWriter.vue
+++ src/components/PostWriter.vue
@@ -1,12 +1,36 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import type { TimelinePost } from '@/posts'
+
+// props
 
 const props = defineProps<{
   post: TimelinePost
 }>()
 
+// variables
+
 const title = ref(props.post.title)
+const content = ref(props.post.markdown)
+const contentEditable = ref<HTMLDivElement>()
+
+// lifecycle hooks
+
+onMounted(() => {
+  if (!contentEditable.value) {
+    throw Error('ContentEditable DOM node was not found')
+  }
+  contentEditable.value.innerText = content.value
+})
+
+// functions
+
+function handleInput(): void {
+  if (!contentEditable.value) {
+    throw Error('ContentEditable DOM node was not found')
+  }
+  content.value = contentEditable.value?.innerText
+}
 </script>
 
 <template>
@@ -15,8 +39,14 @@ const title = ref(props.post.title)
       <div class="field">
         <div class="label">Post title</div>
         <input type="text" class="input" v-model="title" />
-        {{ title }}
       </div>
     </div>
+  </div>
+
+  <div class="columns">
+    <div class="column">
+      <div contenteditable ref="contentEditable" @input="handleInput" />
+    </div>
+    <div class="column">{{ content }}</div>
   </div>
 </template>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -6,7 +6,8 @@ import { DateTime } from 'luxon'
 const post: TimelinePost = {
   id: '-1',
   title: 'Title',
-  created: DateTime.now()
+  created: DateTime.now(),
+  markdown: '## Title'
 }
 </script>
 
