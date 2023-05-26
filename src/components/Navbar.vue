33ee553ffa84da72088791f25274d4ebb52329f6
PhilSchoe <p.schoemacker@gmx.de>
2023-05-26 21:13:04 +0200

    Add modal using teleport

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,20 @@
+<script lang="ts" setup>
+import { useModal } from '../composables/modal';
+
+const modal = useModal()
+</script>
+
 <template>
   <div class="navbar">
-    <div class="nav-end">
+    <div class="navbar-end">
       <div class="buttons">
+        <button class="button" @click.prevent="modal.showModal()">Sign Up</button>
         <RouterLink to="/posts/new" class="button">New Post</RouterLink>
       </div>
     </div>
   </div>
+
+  <Teleport to="#modal">
+    Content to be rendered.
+  </Teleport>
 </template>
