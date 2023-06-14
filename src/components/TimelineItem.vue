46a2e853dbe58f0501ac695902015a600ca8959c
philippschoemacker <p.schoemacker@gmx.de>
2023-06-14 21:21:37 +0200

    Add show post page and dynamic route

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -7,8 +7,11 @@ defineProps<{
 </script>
 
 <template>
-  <a class="panel-block is-flex is-flex-direction-column is-align-items-flex-start">
-    {{ post.title }}
-    {{ post.created.toFormat('d MMM') }}
-  </a>
+  <RouterLink
+    :to="`/posts/${post.id}`"
+    class="panel-block is-flex is-flex-direction-column is-align-items-flex-start"
+  >
+    <a>{{ post.title }}</a>
+    <div>{{ post.created.toFormat('d MMM') }}</div>
+  </RouterLink>
 </template>
---
+++ src/views/ShowPost.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { usePosts } from '@/stores/posts'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const postsStore = usePosts()
+const id = route.params.id as string
+
+const post = postsStore.all.get(id)
+
+if (!post) {
+  throw Error(`Post with id ${id} was not found`)
+}
+</script>
+
+<template>
+  <div class="columns">
+    <div class="columns"></div>
+    <div class="columns is-two-thirds">
+      <h1>{{ post.title }}</h1>
+      <div v-html="post.html" />
+    </div>
+    <div class="columns"></div>
+  </div>
+</template>
