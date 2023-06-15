5e939cec66a56ab1250e2a4471c373037d2f27b5
PhilSchoe <p.schoemacker@gmx.de>
2023-06-15 21:16:12 +0200

    Only show edit button when user is authenticated and authorized

--- src/views/ShowPost.vue
+++ src/views/ShowPost.vue
@@ -1,23 +1,38 @@
 <script setup lang="ts">
 import { usePosts } from '@/stores/posts'
+import { useUsers } from '@/stores/users'
+import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const postsStore = usePosts()
-const id = route.params.id as string
+const usersStore = useUsers()
 
+const id = route.params.id as string
 const post = postsStore.all.get(id)
 
 if (!post) {
   throw Error(`Post with id ${id} was not found`)
 }
+
+const canEdit = computed(() => {
+  if (!usersStore.currentUserId) {
+    return false
+  }
+
+  if (usersStore.currentUserId !== post.authorId) {
+    return false
+  }
+
+  return true
+})
 </script>
 
 <template>
   <div class="columns">
     <div class="column"></div>
     <div class="column is-two-thirds">
-      <RouterLink :to="`/posts/${post.id}/edit`" class="is-link button is-rounded"
+      <RouterLink v-if="canEdit" :to="`/posts/${post.id}/edit`" class="is-link button is-rounded"
         >Edit Post</RouterLink
       >
       <h1>{{ post.title }}</h1>
