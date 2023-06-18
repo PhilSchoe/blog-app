32aeb59a4c39f6dd6709b549c3efd47ed6989c25
PhilSchoe <p.schoemacker@gmx.de>
2023-06-18 20:05:40 +0200

    Solve race condition and add loading bar

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import { router } from './router'
+import { useUsers } from './stores/users'
+import { usePosts } from './stores/posts'
 
 const pinia = createPinia()
 
-createApp(App).use(pinia).use(router).mount('#app')
+const app = createApp(App)
+app.use(pinia)
+
+const usersStore = useUsers()
+const postsStore = usePosts()
+
+Promise.all([usersStore.authenticate(), postsStore.fetchPosts()]).then(() => {
+  app.use(router)
+  app.mount('#app')
+})
