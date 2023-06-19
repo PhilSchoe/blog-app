8e993fe6cad7398606d92250c0d61fb4e9275389
PhilSchoe <p.schoemacker@gmx.de>
2023-06-19 16:41:37 +0200

    Add vitest and first test files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    environment: 'jsdom'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
