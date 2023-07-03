cd447a17b96c5144464b247d1992ff9da0e51b6d
PhilSchoe <p.schoemacker@gmx.de>
2023-07-03 17:03:35 +0200

    Finish UserForm test

--- src/components/__tests__/UserForm.spec.ts
+++ src/components/__tests__/UserForm.spec.ts
@@ -19,6 +19,47 @@ describe('UserForm', () => {
   })
 
   it('runs through the workflow', async () => {
-    // Next mount UserForm
+    const wrapper = mount(UserForm, {
+      global: {
+        plugins: [pinia, router]
+      }
+    })
+
+    const btn = wrapper.find('button')
+    expect(btn.element.disabled).toBe(true)
+
+    expect(wrapper.find('[data-testid="username"]').find('.is-danger').text()).toBe(
+      'This field is required'
+    )
+    expect(wrapper.find('[data-testid="password"]').find('.is-danger').text()).toBe(
+      'This field is required'
+    )
+
+    await wrapper.find('#Username').setValue('user')
+    await wrapper.find('#Password').setValue('password')
+
+    expect(wrapper.find('[data-testid="username"]').find('.is-danger').text()).toBe(
+      'This field must be between 5 and 10'
+    )
+    expect(wrapper.find('[data-testid="password"]').find('.is-danger').text()).toBe(
+      'This field must be between 10 and 40'
+    )
+
+    await wrapper.find('#Username').setValue('username')
+    await wrapper.find('#Password').setValue('password123')
+
+    expect(wrapper.find('[data-testid="username"]').find('.is-danger').exists()).toBe(false)
+    expect(wrapper.find('[data-testid="password"]').find('.is-danger').exists()).toBe(false)
+
+    expect(wrapper.find('button').element.disabled).toBe(false)
+
+    wrapper.find('form').trigger('submit.prevent')
+
+    expect(wrapper.emitted().submit[0]).toEqual([
+      {
+        username: 'username',
+        password: 'password123'
+      }
+    ])
   })
 })
